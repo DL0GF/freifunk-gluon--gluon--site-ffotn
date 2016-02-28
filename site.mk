2a5be7b599848c24ecc43d840d36aa3894b44597
Freifunk <g.thb@pacher.com>
2016-02-28 17:12:23 +0100

    gluon-1602191336; new fastd; ibss + mesh

--- site.mk
+++ site.mk
@@ -8,6 +8,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-15 \
 	gluon-alfred \
 	gluon-respondd \
+	gluon-authorized-keys \
 	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-contact-info \
@@ -19,7 +20,9 @@ GLUON_SITE_PACKAGES := \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
+        gluon-luci-node-role \
 	gluon-luci-portconfig \
+	gluon-luci-private-wifi \
 	gluon-luci-wifi-config \
 	gluon-next-node \
 	gluon-mesh-vpn-fastd \
@@ -36,7 +39,7 @@ GLUON_SITE_PACKAGES := \
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 0.6+exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := r$(shell date '+%y%m%d')
 
 
 ##	GLUON_RELEASE
